34cf2af4e256e4a4619c7293605cb1d33947d365
mittalpriya987 <priya.mittal@kapylon.com>
2024-05-07 00:30:11 +0530

    my portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,152 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2000,
+  delay: 200,
+  //     reset: true
+});
+
+sr.reveal(".home__data, .about__img, .skills__subtitle, .skills__text", {});
+sr.reveal(".home__img, .about__subtitle, .about__text, .skills__img", {
+  delay: 400,
+});
+sr.reveal(".home__social-icon", { interval: 200 });
+sr.reveal(".skills__data, .work__img, .contact__input", { interval: 200 });
+
+// const circle = document.querySelector(".circle");
+
+// document.addEventListener("mousemove", (e) => {
+//   const mouseX = e.clientX;
+//   const mouseY = e.clientY;
+
+//   circle.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
+// });
+
+const cursorInner = document.getElementById("cursor-inner");
+const cursorOuter = document.getElementById("cursor-outer");
+const links = document.querySelectorAll("a,label,button");
+
+document.addEventListener("mousemove", function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorInner.style.left = `${posX}px`;
+  cursorInner.style.top = `${posY}px`;
+
+  // cursorOuter.style.left = `${posX}px`;
+  // cursorOuter.style.top = `${posY}px`;
+
+  cursorOuter.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    { duration: 500, fill: "forwards" }
+  );
+
+  links.forEach((link) => {
+    link.addEventListener("mouseenter", () => {
+      cursorInner.classList.add("hover");
+      cursorOuter.classList.add("hover");
+    });
+    link.addEventListener("mouseleave", () => {
+      cursorInner.classList.remove("hover");
+      cursorOuter.classList.remove("hover");
+    });
+  });
+});
+
+// back to top button
+// function scrolltoTopfunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+// Scroll to top function
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+// Show/hide back to top button based on scroll position
+window.addEventListener("scroll", () => {
+  const backToTopBtn = document.getElementById("backToTopBtn");
+  if (window.scrollY > 100) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+});
+
+function openURL() {
+  var href = "../file/priyaMittal.pdf";
+
+  window.open(href, "_blank");
+}
+
+// submiting form
+function sendEmail() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "mittalpriya2208@gmail.com",
+    Password: "9837125906",
+    To: "mittalpriya2208@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "New Contact Form Enquiry",
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br> Message: " +
+      document.getElementById("message").value,
+  }).then((message) => alert("Email send Successfully"));
+}
